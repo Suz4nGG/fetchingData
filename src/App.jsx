bbc782fdfd5db9bd037d9c12c34660a0fd51e2a5
Suz4nGG <scxg240@gmail.com>
2023-04-20 22:20:10 -0600

    Agregando la opción de abortar la petición

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import './App.css'
 import { API_URL } from './constants'
 import { useFetch } from './hooks/useFetch'
+// import { useFetchController } from './hooks/useFetchController'
+// import { Suspense } from 'react'
 
 function App () {
-  const { data } = useFetch(API_URL)
-  console.log(data)
+  // const APIData = useFetchController(API_URL)
+  const { data, loading, error, handleCancelRequest } = useFetch(API_URL)
+  // const dataRead = APIData.read()
+
   return (
-    <div div className='App'>
-      {
-        !data
-          ? 'url'
-          : data.map(item => (
-            <div key={item.id}>
-              * {item.title}
-            </div>
-          ))
-      }
+    <div className='App'>
+      <h1>Fetching Data</h1>
+      <button onClick={handleCancelRequest}>Cancel Request</button>
+      {error && <li>Error: {error}</li>}
+      {loading && <li>Loading...</li>}
+      {data && data?.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+      {/* <Suspense fallback={<div>Loading...</div>}>
+        <ul className='card'>
+          {dataRead?.map((item) => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+      </Suspense> */}
     </div>
   )
 }
