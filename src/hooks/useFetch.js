80eb478800aff876c07e551ea4f6ed98d367be88
Suz4nGG <scxg240@gmail.com>
2023-04-20 22:19:35 -0600

    Manejando errores y estados de carga de datos

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,13 +2,16 @@ import { useState, useEffect } from 'react'
 
 export const useFetch = (url) => {
   const [data, setData] = useState({})
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
-      .then((data) => {
-        setData(data)
-      })
+      .then((data) => setData(data))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false))
   }, [])
-  console.log('DD', data)
-  return { data }
+
+  return { data, loading, error }
 }
